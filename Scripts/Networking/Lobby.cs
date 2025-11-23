1240643367178a4e1d4ac88c18d7bd2ccd8743f8
Isabell <igr45225@stud.hs-furtwangen.de>
2025-11-23 18:45:48 +0100

    demo

---
+++ Scripts/Networking/Lobby.cs
@@ -0,0 +1,399 @@
+using System.Linq;
+using System.Text.RegularExpressions;
+using Godot;
+using Godot.Collections; //Important to not Confuse with c# System.Collections.Generic
+public partial class Lobby : Node
+{
+    //Static instace that is always Accesable by all scripts
+    public static Lobby Instance { get; private set; }
+
+    // These signals can be connected to by a UI lobby scene or the game scene.
+    [Signal]
+    public delegate void PlayerConnectedEventHandler(int peerId, Dictionary<string, string> playerInfo);
+    [Signal]
+    public delegate void PlayerDisconnectedEventHandler(int peerId);
+    [Signal]
+    public delegate void ServerDisconnectedEventHandler();
+
+    /*
+    Variables for setting up the server
+    */
+    [Export]
+    public int Port = 7000;
+    [Export]
+    private string DefaultServerIP = "127.0.0.1"; // IPv4 localhost
+    [Export]
+    private int MaxConnections = 4;
+
+
+    /* 
+    This will contain player info for every player that is currently connected to the server
+    with the keys being each player's unique IDs.
+    It stores values like this <unique ID, <"Name", "Actual Player name>> "Name" is just the key in the Dictionary
+    */
+    public Dictionary<long, Dictionary<string, string>> _players = new Dictionary<long, Dictionary<string, string>>();
+
+    /*
+    This is the local player info. This should be modified locally
+    before the connection is made. It will be passed to every other peer.
+    For example, the value of "name" can be set to something the player entered in a UI scene.
+    */
+
+    public Dictionary<string, string> _playerInfo = new Dictionary<string, string>()
+    {
+        { "Name", "PlayerName" },
+    };
+
+
+    /*
+    Variables that for example only the server keeps track on
+    Only Relevant in the Section: Methods for handling any other signals for game logic
+    */
+    private Dictionary<long, bool> _playersReady = new Dictionary<long, bool>();
+
+    // These signals can be connected to by a UI lobby scene or the game scene.
+    [Signal]
+    public delegate void EnableStartGameEventHandler();
+    [Signal]
+    public delegate void DisableStartGameEventHandler();
+    [Signal]
+    public delegate void GameStartedEventHandler();
+    [Signal]
+    public delegate void SetClientReadyEventHandler(int playerID);
+    [Signal]
+    public delegate void SetClientNotReadyEventHandler(int playerID);
+    [Signal]
+    public delegate void SendClientStateEventHandler(int playerID, bool ready);
+    [Signal]
+    public delegate void ClientRequestStateEventHandler(int playerID, int targetPlayerID);
+
+    public override void _EnterTree()
+    {
+        Instance = this;
+        Multiplayer.PeerConnected += OnPlayerConnected;
+        Multiplayer.PeerDisconnected += OnPlayerDisconnected;
+        Multiplayer.ConnectedToServer += OnConnectOk;
+        Multiplayer.ConnectionFailed += OnConnectionFail;
+        Multiplayer.ServerDisconnected += OnServerDisconnected;
+    }
+
+
+    /*-------------------------------------------------------------------------
+    Methods for handling creating and joining a Lobby
+    -------------------------------------------------------------------------*/
+    /*
+    Creates a Multiplayer.MultiplayerPeer that connects to a server
+    */
+    public Error JoinGame(string address = "")
+    {
+        if (string.IsNullOrEmpty(address))
+        {
+            address = DefaultServerIP;
+        }
+
+        ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
+        Error error = peer.CreateClient(address, Port);
+
+        if (error != Error.Ok)
+        {
+            return error;
+        }
+
+        Multiplayer.MultiplayerPeer = peer;
+        return Error.Ok;
+    }
+
+    /*
+    Creates a Multiplayer.MultiplayerPeer that is also a server
+    Server always has the the Multiplayer.GetUniqueId() == 1
+    */
+    public Error CreateGame()
+    {
+        ENetMultiplayerPeer peer = new ENetMultiplayerPeer();
+        Error error = peer.CreateServer(Port, MaxConnections);
+
+        if (error != Error.Ok)
+        {
+            return error;
+        }
+
+        Multiplayer.MultiplayerPeer = peer;
+        _players[1] = _playerInfo;
+        EmitSignal(SignalName.PlayerConnected, 1, _playerInfo);
+        return Error.Ok;
+    }
+
+
+    /*
+    Disconnects a Multiplayer.MultiplayerPeer from the lobby
+    */
+    public void RemoveMultiplayerPeer()
+    {
+        // Close server if this instance is the server
+        if (Multiplayer.IsServer())
+        {
+            if (Multiplayer.MultiplayerPeer is ENetMultiplayerPeer enetPeer)
+            {
+                enetPeer.Close();
+            }
+        }
+
+        // Disconnect from server if client
+        Multiplayer.MultiplayerPeer = null;
+
+        // Clear all player info
+        _players.Clear();
+
+        // Emit a signal so the UI or other nodes know the server disconnected
+        EmitSignal(SignalName.ServerDisconnected);
+    }
+
+
+    /*-------------------------------------------------------------------------
+    Methods for handling new Connection of clients with the lobby
+    -------------------------------------------------------------------------*/
+
+    /* 
+    When a peer connects, send them my player info.
+    This Method is called when Signal Multiplayer.PeerConnected is emitted.
+    Every Client and Server calles this method when a new client joins the lobby
+    It transmit the _playerInfo to the newly connected client
+    */
+    private void OnPlayerConnected(long id)
+    {
+        RpcId(id, MethodName.RegisterPlayer, _playerInfo);
+    }
+
+
+    /*
+    This method is Receives _playerInfo from each client in the lobby when this client connects to the lobby
+    */
+
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    private void RegisterPlayer(Dictionary<string, string> newPlayerInfo)
+    {
+        int newPlayerId = Multiplayer.GetRemoteSenderId();
+        _players[newPlayerId] = newPlayerInfo;
+        EmitSignal(SignalName.PlayerConnected, newPlayerId, newPlayerInfo);
+
+        //Debug Message
+        GD.Print("Client: " + Multiplayer.GetUniqueId() + "\t RegisterPlayer: " + newPlayerId);
+    }
+
+    /*
+    This method is Called by the client who connects to the server when connection was successful
+    */
+    private void OnConnectOk()
+    {
+        int peerId = Multiplayer.GetUniqueId();
+        _players[peerId] = _playerInfo;
+        EmitSignal(SignalName.PlayerConnected, peerId, _playerInfo);
+
+        //Debug Message
+        GD.Print("Client: " + peerId + "\t OnConnectOk");
+    }
+
+
+
+
+    /*-------------------------------------------------------------------------
+    Methods for handling diconnections of clients from the lobby
+    -------------------------------------------------------------------------*/
+
+    /*
+    This method is called by each client in the lobby when a client disconnects from the lobby
+    The Parameter ID is the client that disconnects from the lobby
+    */
+    private void OnPlayerDisconnected(long id)
+    {
+        _players.Remove(id);
+
+        if (Multiplayer.IsServer())
+        {
+            _playersReady.Remove(id);
+
+        }
+        EmitSignal(SignalName.PlayerDisconnected, id);
+
+        //Leave also when server disconnects
+        //if(id == 1)
+        //{
+        //    RemoveMultiplayerPeer();
+        //}
+
+        //Debug Message
+        GD.Print("Client: " + Multiplayer.GetUniqueId() + "\t OnPlayerDisconnected: " + id);
+    }
+
+
+    /*
+    This method is called by each client when the server disoconnects
+    */
+    private void OnServerDisconnected()
+    {
+        Multiplayer.MultiplayerPeer = null;
+        _players.Clear();
+        EmitSignal(SignalName.ServerDisconnected);
+
+        //Debug Message
+        GD.Print("Client: " + Multiplayer.GetUniqueId() + "\t OnServerDisconnected");
+    }
+
+    private void OnConnectionFail()
+    {
+        Multiplayer.MultiplayerPeer = null;
+
+        GD.Print("Client: " + Multiplayer.GetUniqueId() + "\t OnConnectionFail");
+    }
+
+
+
+
+
+
+
+    /*-------------------------------------------------------------------------
+    Methods for handling any other signals for game logic
+    -------------------------------------------------------------------------*/
+
+    /*
+    Every client calls this method when a client on the lobby Sets itself to ready
+    */
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    private void PlayerReady()
+    {
+        long senderID = Multiplayer.GetRemoteSenderId();
+
+        //Only Server keeps track if all clients are ready
+        if (Multiplayer.IsServer())
+        {
+            //Check if player is already in dictionary
+            if (_playersReady.ContainsKey(senderID))
+            {
+                //Mark player as ready
+                _playersReady[senderID] = true;
+            }
+            else
+            {
+                //Add new Player to the dictionary
+                _playersReady.Add(senderID, true);
+            }
+
+            //Check if all players in the lobby are ready
+            if (_playersReady.Keys.Count == _players.Keys.Count && !_playersReady.Keys.Except(_players.Keys).Any())
+            {
+                //Check if all values are tru
+                bool allReady = true;
+                foreach (bool val in _playersReady.Values)
+                {
+                    if (val == false)
+                    {
+                        allReady = false;
+                        break;
+                    }
+                }
+
+                //Emit signal when all are ready
+                if (allReady)
+                {
+                    //Emits the signal to the ui manager to enable the start game button
+                    EmitSignal(SignalName.EnableStartGame, null);
+                    GD.Print("All Clients are ready");
+                }
+            }
+        }
+
+        //Check if sender is same as receiver
+        if (senderID == Multiplayer.GetUniqueId())
+        {
+            //Do nothing all changes are already made local
+            return;
+        }
+
+        //Every client updates ui to show which clients are not ready
+        EmitSignal(SignalName.SetClientReady, senderID);
+    }
+
+    /*
+    Every client calls this method when a client on the lobby Sets itself to not ready
+    */
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    private void PlayerNotReady()
+    {
+        long senderID = Multiplayer.GetRemoteSenderId();
+
+        //Only Server keeps track if all clients are ready
+        if (Multiplayer.IsServer())
+        {
+            //Check if player is already in dictionary
+            if (_playersReady.ContainsKey(senderID))
+            {
+                //Mark player as ready
+                _playersReady[senderID] = false;
+            }
+            else
+            {
+                //Add new Player to the dictionary
+                _playersReady.Add(senderID, false);
+            }
+
+            //Hide start button
+            EmitSignal(SignalName.DisableStartGame, null);
+
+        }
+
+        //Check if sender is same as receiver
+        if (senderID == Multiplayer.GetUniqueId())
+        {
+            //Do nothing all changes are already made local
+            return;
+        }
+
+        //Every client updates ui to show which clients are not ready
+        EmitSignal(SignalName.SetClientNotReady, senderID);
+    }
+
+    /*
+    Server call this method when button to start game is pressed
+    */
+    [Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    public void ServerStartGame()
+    {
+        // Only server updates ready states
+        if (Multiplayer.IsServer())
+        {
+            foreach (long key in _playersReady.Keys)
+                _playersReady[key] = false;
+
+            EmitSignal(SignalName.DisableStartGame); // server-only signal
+        }
+
+        // Runs on all peers, including clients
+        EmitSignal(SignalName.GameStarted);
+    }
+
+    /*
+    Server call this method when client request states to be synchronized
+    */
+    [Rpc(MultiplayerApi.RpcMode.Authority, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    public void ServerSendSynchronizeStates(int playerID, bool ready)
+    {
+        // Runs on all peers, including clients
+        EmitSignal(SignalName.SendClientState, playerID, ready);
+    }
+
+    /*
+Client sends this to request a state from the server
+Sends own id + id it request the ready state from
+*/
+    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+    public void ClientRequestReady(int clientPlayerID, int clientRequestID)
+    {
+        if (Multiplayer.IsServer())
+        {
+            //Only Server Responses
+            EmitSignal(SignalName.ClientRequestState, clientPlayerID, clientRequestID);
+        }
+    }
+
+}
---
+++ Scripts/Networking/UIManager.cs
@@ -0,0 +1,301 @@
+using Godot;
+using System;
+using Godot.Collections;
+using System.Net.NetworkInformation;
+
+public partial class UIManager : Node
+{
+    [Export]
+    public CanvasLayer lobbyMenu;
+    [Export]
+    public CanvasLayer hostMenu;
+    [Export]
+    public CanvasLayer startMenu;
+    [Export]
+    public CanvasLayer joinMenu;
+
+    [Export]
+    public Button startButton;
+
+
+    [Export]
+    public VBoxContainer playerInfoContainer;
+
+    [Export]
+    public PackedScene playerInfoTemplate;
+
+    private Dictionary<int, Control> playerInfoData = new Dictionary<int, Control>();
+
+
+    public override void _Ready()
+    {
+        //Hide all menus except the start menu
+        lobbyMenu.Hide();
+        hostMenu.Hide();
+        joinMenu.Hide();
+        startButton.Hide();
+        startMenu.Show();
+
+        //Subscribe to server events
+        Lobby.Instance.PlayerConnected += AddPlayerInfoToLobbyMenu;
+        Lobby.Instance.PlayerDisconnected += RemovePlayerInfoFromLobbyMenu;
+        Lobby.Instance.SetClientReady += SetClientToPressed;
+        Lobby.Instance.SetClientNotReady += SetClientNotPressed;
+        Lobby.Instance.EnableStartGame += ShowStartButton;
+        Lobby.Instance.DisableStartGame += HideStartButton;
+        Lobby.Instance.GameStarted += GameStarted;
+        Lobby.Instance.SendClientState += GetRequestedReadyState;
+        Lobby.Instance.ClientRequestState += ServerSendReadyState;
+    }
+
+
+    public void StartHostingServer()
+    {
+        //Get the name entered in the start menu
+        LineEdit nameField = startMenu.GetNode<LineEdit>("Name Input");
+        string name = nameField.Text;
+        Lobby.Instance._playerInfo["Name"] = name;
+
+
+        //Try starting a server
+        Error error = Lobby.Instance.CreateGame();
+        if (error != Error.Ok)
+        {
+            GD.Print("Failed Creating a Server");
+            return;
+        }
+
+        //When user starts hosting hide irelevant menus
+        startMenu.Hide();
+        joinMenu.Hide();
+        startButton.Hide();
+        //Show relevant menus
+        lobbyMenu.Show();
+        hostMenu.Show();
+
+        //Assign ip address and port to the menu elements
+        string serverIp = IP.GetLocalAddresses()[1]; //Look in Documentation to determen the correct IP of the server in the Array of IPs
+        string serverPort = Lobby.Instance.Port.ToString();
+
+        //Add text to line edit
+        LineEdit ipField = hostMenu.GetNode<LineEdit>("IP Field");
+        LineEdit portField = hostMenu.GetNode<LineEdit>("Port Field");
+        ipField.Text = serverIp;
+        portField.Text = serverPort;
+
+        //Add own player info to the container
+        AddPlayerInfoToLobbyMenu(Multiplayer.MultiplayerPeer.GetUniqueId(), Lobby.Instance._playerInfo);
+    }
+
+
+    public void StartJoiningServer()
+    {
+        //Hide menus not relevant for joining a server
+        startMenu.Hide();
+        lobbyMenu.Hide();
+        hostMenu.Hide();
+        startButton.Hide();
+
+        //Show menu relevant for joining
+        joinMenu.Show();
+    }
+
+    public void JoinServer()
+    {
+        //Get the IP, port and name from the input fields
+        LineEdit ipField = joinMenu.GetNode<LineEdit>("IP Input");
+        LineEdit portField = joinMenu.GetNode<LineEdit>("Port Input");
+        LineEdit nameField = startMenu.GetNode<LineEdit>("Name Input");
+
+        string ip = ipField.Text;
+        int port = -1;
+        Int32.TryParse(portField.Text, out port);
+        string name = nameField.Text;
+
+
+        //Assign values to the lobby instance
+        if (port != -1)
+        {
+            Lobby.Instance.Port = port;
+        }
+        Lobby.Instance._playerInfo["Name"] = name;
+
+        //Try Joining the Lobby
+        Error error = Lobby.Instance.JoinGame(ip);
+        if (error != Error.Ok)
+        {
+            GD.Print("Joining Server Failed");
+            GD.Print("Joined Server");
+            lobbyMenu.Hide();
+        }
+
+        //Hide menus that are no longer relevant
+        joinMenu.Hide();
+        startMenu.Hide();
+        hostMenu.Hide();
+        startButton.Hide();
+        //Show relevant menu
+        lobbyMenu.Show();
+
+        //Add own player info to the container
+        AddPlayerInfoToLobbyMenu(Multiplayer.MultiplayerPeer.GetUniqueId(), Lobby.Instance._playerInfo);
+    }
+
+    public void CheckBoxToggled(bool marked)
+    {
+        if (marked)
+        {
+            MarkReady();
+        }
+        else
+        {
+            MarkNotReady();
+        }
+    }
+
+    private void MarkReady()
+    {
+        Lobby.Instance.Rpc(Lobby.MethodName.PlayerReady); // Tell the server that this peer is ready
+    }
+
+
+    private void MarkNotReady()
+    {
+        Lobby.Instance.Rpc(Lobby.MethodName.PlayerNotReady); // Tell the server that this peer is not ready
+    }
+
+    private void ShowStartButton()
+    {
+        startButton.Show();
+    }
+
+    private void HideStartButton()
+    {
+        startButton.Hide();
+    }
+
+    public void StartGame()
+    {
+        //Send over Network that game started
+        Lobby.Instance.Rpc(Lobby.MethodName.ServerStartGame); // Tell the all clients that the game starts now.
+
+        startButton.Hide();
+    }
+
+
+    public void AddPlayerInfoToLobbyMenu(int playerID, Dictionary<string, string> newPlayerInfo)
+    {
+        // Create an instance of the template UI element
+        Control newPlayerInfoTemplate = playerInfoTemplate.Instantiate<Control>();
+
+        //Assign values to the control
+        newPlayerInfoTemplate.GetNode<LineEdit>("Client Name").Text = newPlayerInfo["Name"];
+        newPlayerInfoTemplate.GetNode<CheckBox>("Client Ready").SetPressedNoSignal(false);
+
+        if (playerID == Multiplayer.GetUniqueId())
+        {
+            newPlayerInfoTemplate.GetNode<CheckBox>("Client Ready").Disabled = false;
+            newPlayerInfoTemplate.GetNode<CheckBox>("Client Ready").Toggled += CheckBoxToggled;
+        }
+        else
+        {
+            newPlayerInfoTemplate.GetNode<CheckBox>("Client Ready").Disabled = true;
+        }
+
+
+        //Store the value in the dictonary
+        playerInfoData.Add(playerID, newPlayerInfoTemplate);
+        // Add to the container (VBoxContainer)
+        playerInfoContainer.AddChild(newPlayerInfoTemplate);
+
+        //Server sends Data to new client to synchronise ready values with the new client
+        Lobby.Instance.Rpc(Lobby.MethodName.ClientRequestReady, Multiplayer.GetUniqueId(), playerID);
+    }
+
+
+    public void RemovePlayerInfoFromLobbyMenu(int playerID)
+    {
+        //Delete the template from the scene
+        playerInfoContainer.RemoveChild(playerInfoData[playerID]);
+
+        //Remove the player Info data from the dictionary
+        playerInfoData.Remove(playerID);
+    }
+
+    public void SetClientToPressed(int playerID)
+    {
+        Control playerInfoTemplate = playerInfoData[playerID];
+        playerInfoTemplate.GetNode<CheckBox>("Client Ready").SetPressedNoSignal(true);
+    }
+
+    public void SetClientNotPressed(int playerID)
+    {
+        Control playerInfoTemplate = playerInfoData[playerID];
+        playerInfoTemplate.GetNode<CheckBox>("Client Ready").SetPressedNoSignal(false);
+    }
+
+
+
+    //Called by a signal from the server
+    public void GameStarted()
+    {
+        //Loop over each client and set it to not ready
+        foreach (int key in playerInfoData.Keys)
+        {
+            SetClientNotPressed(key);
+        }
+    }
+
+
+    public void ResetToStart()
+    {
+        foreach (int key in playerInfoData.Keys)
+        {
+            RemovePlayerInfoFromLobbyMenu(key);
+        }
+
+        playerInfoData.Clear();
+
+
+        //When connected to server leave the server
+        if (Multiplayer.MultiplayerPeer != null)
+        {
+            Lobby.Instance.RemoveMultiplayerPeer();
+        }
+
+
+        //Show menus in start layout
+        lobbyMenu.Hide();
+        hostMenu.Hide();
+        joinMenu.Hide();
+        startButton.Hide();
+        startMenu.Show();
+    }
+
+
+
+
+    public void ServerSendReadyState(int playerID, int targetPlayerID)
+    {
+        //Get the state of the target client
+        Control playerInfoTemplate = playerInfoData[targetPlayerID];
+        bool ready = playerInfoTemplate.GetNode<CheckBox>("Client Ready").ButtonPressed;
+
+        if (Multiplayer.IsServer())
+        {
+            Lobby.Instance.RpcId(playerID, Lobby.MethodName.ServerSendSynchronizeStates, targetPlayerID, ready);
+        }
+    }
+
+    public void GetRequestedReadyState(int playerID, bool ready)
+    {
+        if (ready)
+        {
+            SetClientToPressed(playerID);
+        }
+        else
+        {
+            SetClientNotPressed(playerID);
+        }
+    }
+}
